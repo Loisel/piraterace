37168859e4db65f224e5569ad7737e8a8598985b
Alois Dirnaichner <alodi@directbox.com>
2021-04-18 23:38:58 +0200

    Add simple game model.

--- roboserv/models.py
+++ roboserv/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
-# Create your models here.
+class Game(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+
+    player_count = models.PositiveSmallIntegerField(default=8)
+    player_names = ArrayField(models.CharField(max_length=20, blank=True))
+    player_pos = ArrayField(
+        ArrayField(models.PositiveSmallIntegerField(), size=2))
+    player_moves = ArrayField(models.CharField(max_length=5, blank=True))
+
