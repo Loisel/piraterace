756d73629e26e5672dba0944ec0a1a57e6b036b4
Johannes Dirnaichner <j.dirnaichner@tum.de>
2021-05-31 14:37:29 +0200

    funktionen in views einheitlich auf englisch

--- roboserv/urls.py
+++ roboserv/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:game_id>/player/<int:player_id>', views.serviert, name='serviere'),
+    path('<int:game_id>/player/<int:player_id>', views.playercards, name='playercards'),
     path('<int:game_id>/deal', views.deal, name='deal'),
 ]
--- roboserv/views.py
+++ roboserv/views.py
@@ -10,7 +10,7 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello, world. You're at the serv index.")
     
-def serviert(request, game_id, player_id):
+def playercards(request, game_id, player_id):
     game = Game.objects.get(id=game_id)
     player = Player.objects.get(id=player_id)
     response = "You're looking at the cards of " + player.name + " : " + player.cards
