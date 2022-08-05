55b23b3c7d5c7265359689c3b36a88c9adc2693c
Fabian Jakub <fabian@jakub.com>
2022-08-05 03:07:45 +0200

    add update Event to load gameinfo data in a loop

--- frontend/src/app/game/game.component.ts
+++ frontend/src/app/game/game.component.ts
@@ -1,7 +1,7 @@
 import { IonicModule } from '@ionic/angular';
 import { Component, OnInit } from '@angular/core';
-import { HttpService } from '../http.service'
-import { Router, ActivatedRoute } from '@angular/router'
+import { HttpService } from '../http.service';
+import { Router, ActivatedRoute } from '@angular/router';
 import { interval } from 'rxjs';
 import Phaser from 'phaser';
 import { environment } from '../../environments/environment';
@@ -12,7 +12,6 @@ import { environment } from '../../environments/environment';
   providers: [HttpService],
   styleUrls: ['./game.component.scss'],
 })
-
 export class GameComponent implements OnInit {
   phaserGame: Phaser.Game;
   config: Phaser.Types.Core.GameConfig;
@@ -21,35 +20,42 @@ export class GameComponent implements OnInit {
     private httpService: HttpService,
     private route: ActivatedRoute,
     private router: Router
-  ) {
-  }
+  ) {}
 
   ngOnInit() {
-    let id = +this.route.snapshot.paramMap.get("game_id");
+    this.load_gameinfo().subscribe(
+      (gameinfo) => {
+        console.log('Game:', gameinfo);
+        this.gameinfo = gameinfo;
+        this.config = {
+          type: Phaser.AUTO,
+          physics: { default: 'None' },
+          scale: {
+            mode: Phaser.Scale.FIT,
+            parent: 'game',
+            width: this.gameinfo.map.width * this.gameinfo.map.tilewidth,
+            height: this.gameinfo.map.height * this.gameinfo.map.tileheight,
+          },
+        };
 
-    this.httpService.getGame(id).subscribe(gameinfo => {
-      console.log("Game:", gameinfo);
-      this.gameinfo = gameinfo;
-      this.config = {
-        type: Phaser.AUTO,
-        physics: {},
-        scale: {
-          mode: Phaser.Scale.FIT,
-          parent: 'game',
-          width: this.gameinfo.map.width * this.gameinfo.map.tilewidth,
-          height: this.gameinfo.map.height * this.gameinfo.map.tileheight,
-        },
-      };
-
-      this.config.scene = new GameScene(this.config, this)
-      this.phaserGame = new Phaser.Game(this.config);
-    });
+        this.config.scene = new GameScene(this.config, this);
+        this.phaserGame = new Phaser.Game(this.config);
+      },
+      (err) => console.error(err),
+      () => console.log('observable complete')
+    );
   }
 
+  load_gameinfo() {
+    let id = +this.route.snapshot.paramMap.get('game_id');
+    return this.httpService.getGame(id);
+  }
 }
 
 class GameScene extends Phaser.Scene {
+  updateTimer: Phaser.Time.TimerEvent;
   boats: any = {};
+  last_played_action: number = 0;
   component: any = null;
   constructor(config, component) {
     super(config);
@@ -57,69 +63,105 @@ class GameScene extends Phaser.Scene {
   }
 
   preload() {
-    console.log("Component", this.component);
+    console.log('Component', this.component);
     this.load.image('tileset', `${environment.STATIC_URL}/maps/wateru.png`);
-    this.load.tilemapTiledJSON('tilemap', `${environment.STATIC_URL}/maps/${this.component.gameinfo.mapfile}`);
-    this.load.spritesheet('boat',
-        `${environment.STATIC_URL}/sprites/boat.png`,
-        { frameWidth: 24, frameHeight: 72 }
+    this.load.tilemapTiledJSON(
+      'tilemap',
+      `${environment.STATIC_URL}/maps/${this.component.gameinfo.mapfile}`
+    );
+    this.load.spritesheet(
+      'boat',
+      `${environment.STATIC_URL}/sprites/boat.png`,
+      { frameWidth: 24, frameHeight: 72 }
     );
   }
 
-  play_actionstack() {
+  async delay(ms: number) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+
+  play_actionstack(animation_time_ms: number) {
     let GI = this.component.gameinfo;
     let actionstack = this.component.gameinfo.actionstack;
-    actionstack.forEach( (action) => {
-      console.log(action);
-      if (action.target > 0) {
-        let boat = this.boats[action.target];
-        if (action.key === "rotate") {
-          boat.angle += 90 * action.val;
-        }
-        if (action.key === "move_x") {
-          boat.x += action.val * GI.map.tilewidth;
-        }
-        if (action.key === "move_y") {
-          boat.y += action.val * GI.map.tileheight;
-        }
-      }
 
-    });
+    console.log(
+      'this.last_played_action',
+      this.last_played_action,
+      actionstack.length
+    );
+
+    for (let i = this.last_played_action; i < actionstack.length; i++) {
+      let action = actionstack[i];
+      this.delay((i - this.last_played_action) * animation_time_ms).then(
+        (any) => {
+          console.log(action);
+
+          if (action.target > 0) {
+            let boat = this.boats[action.target];
+            if (action.key === 'rotate') {
+              boat.angle += 90 * action.val;
+            }
+            if (action.key === 'move_x') {
+              boat.x += action.val * GI.map.tilewidth;
+            }
+            if (action.key === 'move_y') {
+              boat.y += action.val * GI.map.tileheight;
+            }
+          }
+        }
+      );
+    }
+    this.last_played_action = actionstack.length;
   }
 
   create() {
     let GI = this.component.gameinfo;
 
     // create the Tilemap
-    const map = this.make.tilemap({ key: 'tilemap', tileWidth: 16, tileHeight: 16})
+    const map = this.make.tilemap({
+      key: 'tilemap',
+      tileWidth: 16,
+      tileHeight: 16,
+    });
 
     // add the tileset image we are using
-    const tileset = map.addTilesetImage('tileset1', 'tileset')
+    const tileset = map.addTilesetImage('tileset1', 'tileset');
 
     // create the layers we want in the right order
     map.createLayer('Background', tileset, 0, 0);
 
-    Object.entries(GI.players).forEach( ([playerid, player]) => {
-        console.log(playerid, player);
-        var boat = this.add.sprite(
-          (player['start_pos_x']-.5) * GI.map.tilewidth,
-          (player['start_pos_y']-.5) * GI.map.tileheight,
-          "boat");
-          //set the width of the sprite
-          boat.displayHeight = GI.map.tileheight;
-          //scale evenly
-          boat.scaleX = boat.scaleY;
-          boat.angle = player['start_direction'] * 90;
-        this.boats[playerid] = boat;
-      }
-    );
+    Object.entries(GI.players).forEach(([playerid, player]) => {
+      console.log(playerid, player);
+      var boat = this.add.sprite(
+        (player['start_pos_x'] - 0.5) * GI.map.tilewidth,
+        (player['start_pos_y'] - 0.5) * GI.map.tileheight,
+        'boat'
+      );
+      //set the width of the sprite
+      boat.displayHeight = GI.map.tileheight * 1.5;
+      //scale evenly
+      boat.scaleX = boat.scaleY;
+      boat.angle = player['start_direction'] * 90;
+      this.boats[playerid] = boat;
+    });
 
+    this.play_actionstack(5); // play the first action stack really quickly in case user does a reload
 
-    this.play_actionstack();
+    this.updateTimer = this.time.addEvent({
+      callback: this.updateEvent,
+      callbackScope: this,
+      delay: 1000, // 1000 = 1 second
+      loop: true,
+    });
   }
 
-  update() {
+  updateEvent(): void {
+    this.component.load_gameinfo().subscribe((gameinfo) => {
+      console.log('UpdateEvent:', gameinfo);
+      this.component.gameinfo = gameinfo;
+      this.play_actionstack(500);
+    });
   }
+
+  update() {}
 }
-
-
