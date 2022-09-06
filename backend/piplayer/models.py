559efeb1e43d04db4edee949cc62930ddf60cbaa
Fabian Jakub <fabian@jakub.com>
2022-09-06 13:02:10 +0200

    fix issue where user account would be deleted if attached game is removed

--- backend/piplayer/models.py
+++ backend/piplayer/models.py
@@ -13,7 +13,7 @@ class Account(models.Model):
     start_loc_y = models.PositiveSmallIntegerField(null=True, blank=True)
     start_direction = models.PositiveIntegerField(null=True, blank=True)
     # game_lobby = models.ForeignKey(GameConfig, on_delete=models.CASCADE)
-    game = models.ForeignKey(BaseGame, on_delete=models.CASCADE, null=True, blank=True)
+    game = models.ForeignKey(BaseGame, on_delete=models.SET_NULL, null=True, blank=True)
     deck = ArrayField(models.IntegerField(null=True, blank=True), null=True, blank=True)
 
     next_card = models.PositiveSmallIntegerField(default=0)
