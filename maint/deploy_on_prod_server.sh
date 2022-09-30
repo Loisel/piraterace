0b72613b79feafa36ffbbb98ddb959c38623d9dc
LeChuck <root@dev.piraterace.com>
2022-09-30 12:05:23 +0000

    run deploy script in background

--- maint/deploy_on_prod_server.sh
+++ maint/deploy_on_prod_server.sh
@@ -4,8 +4,8 @@ PROJECT_ROOT="$(readlink -f $SCRIPTDIR/../)"
 
 $PROJECT_ROOT/maint/backend_migrate.sh
 $PROJECT_ROOT/maint/backend_collectstatic.sh
-$PROJECT_ROOT/frontend_npm_install.sh
+$PROJECT_ROOT/maint/frontend_npm_install.sh
 
 cd $PROJECT_ROOT
 docker-compose run --rm frontend sh -c "ionic build --prod"
-docker-compose -f $PROJECT_ROOT/docker-compose-prod.yml up
+docker-compose -f $PROJECT_ROOT/docker-compose-prod.yml up -d
