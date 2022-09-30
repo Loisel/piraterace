efde2ea4b5549547c1c5095e67d0896b1a1729fc
LeChuck <root@dev.piraterace.com>
2022-09-30 11:02:11 +0000

    add more steps to deploy script to run the setup from scratch

--- maint/deploy_on_prod_server.sh
+++ maint/deploy_on_prod_server.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+PROJECT_ROOT="$(readlink -f $SCRIPTDIR/../)"
+
+$PROJECT_ROOT/maint/backend_migrate.sh
+$PROJECT_ROOT/maint/backend_collectstatic.sh
+$PROJECT_ROOT/frontend_npm_install.sh
 
 docker-compose run --rm frontend sh -c "ionic build --prod"
 docker-compose -f docker-compose-prod.yml up
