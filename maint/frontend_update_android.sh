6e4b4f2fab95d4c75e4f093e715e56211e2370a1
Fabian Jakub <fabian@jakub.com>
2022-11-04 00:34:22 +0100

    add build instructions for android apks

--- maint/frontend_update_android.sh
+++ maint/frontend_update_android.sh
@@ -6,3 +6,7 @@ cd $PROJECT_ROOT
 docker-compose run --rm frontend sh -c "ionic build --prod"
 docker-compose run --rm frontend sh -c "ionic cap sync --prod"
 $PROJECT_ROOT/maint/frontend_chown.sh
+
+echo "Run ~/android-studio/bin/studio.sh to build apks"
+
+scp frontend/android/app/build/outputs/apk/debug/app-debug.apk root@dev.piraterace.com:/opt/piraterace/volumes/static_volume/
