9b3f52d1f2ba42ef1af24564cd9106aea5607b47
Alois Dirnaichner <alodi@directbox.com>
2022-12-08 13:04:26 +0100

    Add modal to select cannon direction.

--- frontend/src/app/game/game.component.ts
+++ frontend/src/app/game/game.component.ts
@@ -1,4 +1,5 @@
 import { IonicModule } from '@ionic/angular';
+import { IonModal } from '@ionic/angular';
 import { ToastController } from '@ionic/angular';
 import { AlertController } from '@ionic/angular';
 import { Platform } from '@ionic/angular';
@@ -40,6 +41,7 @@ export class GameComponent {
 
   @ViewChild('cards_menu', { read: ElementRef }) cards_menu: ElementRef;
   @ViewChild('tools_menu', { read: ElementRef }) tools_menu: ElementRef;
+  @ViewChild(IonModal) cannonModal: IonModal;
 
   constructor(
     private httpService: HttpService,
