80dc72e55a782a111b992a1e9c74b9c01eaa5b6e
Alois Dirnaichner <alodi@directbox.com>
2022-12-13 00:35:48 +0100

    Remove leftovers on home component.

--- frontend/src/app/home/home.page.ts
+++ frontend/src/app/home/home.page.ts
@@ -1,54 +1,14 @@
-//import { Component } from '@angular/core';
-//
-//@Component({
-//  selector: 'app-home',
-//  templateUrl: 'home.page.html',
-//  styleUrls: ['home.page.scss'],
-//})
-//export class HomePage {
-//
-//  constructor() {}
-//
-//}
 
-import { Component, OnInit } from '@angular/core';
-import Phaser from 'phaser';
+import { Component } from '@angular/core';
 
-class GameScene extends Phaser.Scene {
-  constructor(config) {
-    super(config);
-  }
-
-  preload() {
-    this.add.text(0.5, 0.5, 'Huhu');
-  }
-
-  create() {}
-
-  update() {}
-}
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage implements OnInit {
-  phaserGame: Phaser.Game;
-  config: Phaser.Types.Core.GameConfig;
+export class HomePage {
 
-  constructor() {
-    this.config = {
-      type: Phaser.AUTO,
-      width: 800,
-      height: 600,
-      physics: {},
-      parent: 'game',
-      scene: GameScene,
-    };
-  }
+  constructor() {}
 
-  ngOnInit(): void {
-    this.phaserGame = new Phaser.Game(this.config);
-  }
 }
